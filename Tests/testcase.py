ee19bb85a50eccd9c5b066f45b59979eee0dc315
ad <adixit@amits-macbookpro2.firstfuel.lan>
2019-03-28 14:17:12 -0400

    Login / Logout Test

--- Tests/testcase.py
+++ Tests/testcase.py
@@ -9,7 +9,7 @@ class SearchText(unittest.TestCase):
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
         self.driver.get("https://admin-uat.firstfuelsoftware.net/ff_auth/login")
-    def test_search_by_text(self):
+    def test_login(self):
         self.search_field = self.driver.find_element_by_name("username")
         self.search_field.send_keys("amitd")
         self.search_field = self.driver.find_element_by_name("password")
@@ -18,7 +18,19 @@ class SearchText(unittest.TestCase):
         text="logout"
         element = self.driver.find_element_by_xpath('//a[contains(@href, "%s")]' % text)
         self.assertEqual("logout", element.text.encode('ascii', 'ignore').lower())
+
+    def test_logout(self):
+        self.search_field = self.driver.find_element_by_name("username")
+        self.search_field.send_keys("amitd")
+        self.search_field = self.driver.find_element_by_name("password")
+        self.search_field.send_keys("Test1234$")
+        self.search_field.submit()
+        element = self.driver.find_element_by_xpath('//a[contains(@href, "%s")]' % "logout")
         element.click()
+        button = self.driver.find_elements_by_xpath("//input[@value='Sign in']")
+
+        self.assertTrue(button)
+
     def tearDown(self):
         self.driver.quit()
 
