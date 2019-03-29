1d62c71c34a6d5ac7e6351963bfeebf8af0ced8f
adixit76 <amit1976@gmail.com>
2019-03-28 22:47:56 -0400

    Add changes to read from config file

--- Tests/testcase.py
+++ Tests/testcase.py
@@ -1,34 +1,41 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import configparser
 
 class SearchText(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome("/usr/local/bin/WebDriver/chromedriver")
+        self.driver = webdriver.Chrome(getConfig('chromedriver','PATH'))
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
-        self.driver.get("https://admin-uat.firstfuelsoftware.net/ff_auth/login")
+        self.driver.get(getConfig('EndPoint','AdminURL'))
+    def getConfig(section,key):
+        config = configparser.ConfigParser()
+        config.read('APIConfig.ini')
+        result=config[section][key]
     def test_login(self):
+        userName=getConfig('Credentials','login')
+        password=getConfig('Credentials','password')
         self.search_field = self.driver.find_element_by_name("username")
-        self.search_field.send_keys("amitd")
+        self.search_field.send_keys(userName)
         self.search_field = self.driver.find_element_by_name("password")
-        self.search_field.send_keys("Test1234$")
+        self.search_field.send_keys(password)
         self.search_field.submit()
         text="logout"
         element = self.driver.find_element_by_xpath('//a[contains(@href, "%s")]' % text)
         self.assertEqual("logout", element.text.encode('ascii', 'ignore').lower())
 
     def test_logout(self):
+        userName=getConfig('Credentials','login')
+        password=getConfig('Credentials','password')
         self.search_field = self.driver.find_element_by_name("username")
-        self.search_field.send_keys("amitd")
+        self.search_field.send_keys(userName)
         self.search_field = self.driver.find_element_by_name("password")
-        self.search_field.send_keys("Test1234$")
+        self.search_field.send_keys(password)
         self.search_field.submit()
         element = self.driver.find_element_by_xpath('//a[contains(@href, "%s")]' % "logout")
         element.click()
         button = self.driver.find_elements_by_xpath("//input[@value='Sign in']")
-
         self.assertTrue(button)
 
     def tearDown(self):
