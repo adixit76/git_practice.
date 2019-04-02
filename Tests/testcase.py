8a140297b51785295cf27b95c3f8b39616d453ae
adixit76 <amit1976@gmail.com>
2019-04-02 16:52:51 -0400

    Updated Test with reading from config File

--- Tests/testcase.py
+++ Tests/testcase.py
@@ -1,33 +1,34 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import configparser
+from settings import ConfigSettings
 
-class SearchText(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome(getConfig('chromedriver','PATH'))
+class UserLogin(unittest.TestCase):
+    cfg = ConfigSettings("config.ini")
+
+    @classmethod
+    def setUpClass(self):
+        driverPath=self.cfg.getConfigSetting('BrowserType','driver')
+        self.driver = webdriver.Chrome(driverPath)
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
-        self.driver.get(getConfig('EndPoint','AdminURL'))
-    def getConfig(section,key):
-        config = configparser.ConfigParser()
-        config.read('APIConfig.ini')
-        result=config[section][key]
+        self.driver.get(self.cfg.getConfigSetting('EndPoint','AdminURL'))
+
     def test_login(self):
-        userName=getConfig('Credentials','login')
-        password=getConfig('Credentials','password')
+        userName=self.cfg.getConfigSetting('Credentials','login')
+        password=self.cfg.getConfigSetting('Credentials','password')
         self.search_field = self.driver.find_element_by_name("username")
         self.search_field.send_keys(userName)
         self.search_field = self.driver.find_element_by_name("password")
         self.search_field.send_keys(password)
         self.search_field.submit()
-        text="logout"
-        element = self.driver.find_element_by_xpath('//a[contains(@href, "%s")]' % text)
+        element = self.driver.find_element_by_xpath('//a[contains(@href, "%s")]' % "logout")
         self.assertEqual("logout", element.text.encode('ascii', 'ignore').lower())
+        element.click()
 
     def test_logout(self):
-        userName=getConfig('Credentials','login')
-        password=getConfig('Credentials','password')
+        userName=self.cfg.getConfigSetting('Credentials','login')
+        password=self.cfg.getConfigSetting('Credentials','password')
         self.search_field = self.driver.find_element_by_name("username")
         self.search_field.send_keys(userName)
         self.search_field = self.driver.find_element_by_name("password")
@@ -37,9 +38,10 @@ class SearchText(unittest.TestCase):
         element.click()
         button = self.driver.find_elements_by_xpath("//input[@value='Sign in']")
         self.assertTrue(button)
-
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         self.driver.quit()
 
 if __name__ == '__main__':
+
     unittest.main()
